d086f46867bd345b5895afe21d87eb0e24e7e93a
Brian Zbriger <brian.zbr@gmail.com>
2024-11-18 18:06:15 -0500

    add org-agenda-files to init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -11,6 +11,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(inhibit-startup-screen t)
+ '(org-agenda-files '("~/my/organice/brain.org"))
  '(package-selected-packages '(magit)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
