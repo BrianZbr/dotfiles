26ad6ae985934eed50b91845a2c34d99fb661232
Brian Zbriger <brian.zbr@gmail.com>
2024-12-02 10:36:55 -0500

    dired kill buffers

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -12,6 +12,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(dired-kill-when-opening-new-dired-buffer t)
  '(inhibit-startup-screen t)
  '(org-agenda-files '("~/my/organice/brain.org"))
  '(org-capture-templates
