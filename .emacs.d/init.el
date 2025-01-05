2260f071c20ad339793a8ef002392f40d8fc1adc
Brian Zbriger <brian.zbr@gmail.com>
2025-01-05 18:19:24 -0500

    rename brain to ipara, add capture templates

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -2,6 +2,7 @@
 (menu-bar-mode -1)
 
 (global-set-key (kbd "C-c a") 'org-agenda)
+(global-set-key (kbd "C-c c") 'org-capture)
 
 ; keep folder clean
 (setq auto-save-file-name-transforms
@@ -14,11 +15,14 @@
  ;; If there is more than one, they won't work right.
  '(dired-kill-when-opening-new-dired-buffer t)
  '(inhibit-startup-screen t)
- '(org-agenda-files '("~/my/organice/brain.org"))
+ '(org-agenda-files '("~/my/organice/ipara.org"))
  '(org-capture-templates
    '(("t" "Inbox TODO" entry
-      (file+headline "~/my/organice/brain.org" "Inbox 📥")
-      "TODO")))
+      (file+headline "~/my/organice/ipara.org" "Inbox 📥")
+      "TODO")
+     ("j" "Journal entry" entry
+      (file+olp+datetree "~/my/organice/journal.org")
+      "* %?" :jump-to-captured t)))
  '(package-selected-packages '(magit)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
